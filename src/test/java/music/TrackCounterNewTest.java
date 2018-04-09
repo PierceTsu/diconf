84c297990660dfc2e5985e0926ee5a06d734cf6a
patrick <piercetsu@gmail.com>
2018-04-09 22:49:53 +0800

    test xml config aop

---
+++ src/test/java/music/TrackCounterNewTest.java
@@ -0,0 +1,48 @@
+package music;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Project : diconf
+ * @Package Name : music
+ * @Description: TODO
+ * @Author : piercetsu@gmail.com
+ * @Create Date: 2018-04-09 21:10
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TrackCounterConfigNew.class)
+public class TrackCounterNewTest {
+
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+    @Autowired
+    private CompactDisc cd;
+
+    @Autowired
+    private TrackCounterNew counter;
+
+    @Test
+    public void testTrackCounter() {
+
+        cd.playTrack(1);
+        cd.playTrack(2);
+        cd.playTrack(2);
+        cd.playTrack(2);
+        cd.playTrack(3);
+        cd.playTrack(3);
+
+        assertEquals(1, counter.getPlayCount(1));
+        assertEquals(3, counter.getPlayCount(2));
+        assertEquals(2, counter.getPlayCount(3));
+        assertEquals(0, counter.getPlayCount(4));
+    }
+}
